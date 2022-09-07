a1f4ee548427084aadd3255e122c1b20fda31865
Vanessa Lutz <84334226+vanessalutz@users.noreply.github.com>
2022-09-07 09:51:59 -0700

    Update sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Rails.application.config.before_configuration do
-  Sentry.init do |config|
+ # Sentry.init do |config|
   # config.breadcrumbs_logger = [:active_support_logger]
 
   # To activate performance monitoring, set one of these options.
@@ -8,8 +8,8 @@ Rails.application.config.before_configuration do
   # or
   # config.traces_sampler = lambda do |context|
   # true
-    config.silence_ready = true
-    config.environments = %w[production rnd-testing scaling]
-    config.sanitize_http_headers = %w[Authorization]
-  end
+  #  config.silence_ready = true
+  #  config.environments = %w[production rnd-testing scaling]
+  #  config.sanitize_http_headers = %w[Authorization]
+ # end
 end
